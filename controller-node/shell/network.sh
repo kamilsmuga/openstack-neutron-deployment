f19d35c2caee8d9832955cf16ce40bb5ecc82d4a
Kamil Smuga <smugakamil@gmail.com>
2014-05-05 23:08:40 +0100

    removed default gateway unnecessary reconfiguration

--- controller-node/shell/network.sh
+++
@@ -1,7 +0,0 @@
-route -n | grep 10.0.0.1
-rc=$?
-if [[ $rc != 0 ]] ; then
-	route add default gw 10.0.0.1 eth1
-fi
-
-
--- network-node/shell/network.sh
+++ network-node/shell/network.sh
@@ -5,9 +5,3 @@ if [[ $rc != 0 ]] ; then
 	/usr/bin/perl -p -i -e "s/address 10.0.2.21/up ip link set dev \\\$IFACE up \n\tdown ip link set dev \\\$IFACE down/g" /etc/network/interfaces
 	/etc/init.d/networking restart
 fi
-
-route -n | grep 10.0.0.1
-rc=$?
-if [[ $rc != 0 ]] ; then
-        route add default gw 10.0.0.1 eth1
-fi
